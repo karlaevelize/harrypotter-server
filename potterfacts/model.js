22130ea6f4e632c2a04f8c5097c1e52fd3c7f802
karaevelize <k.evelize@gmail.com>
2020-01-10 15:54:01 +0100

    small fix, sequelize type from string to text

--- potterfacts/model.js
+++ potterfacts/model.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const PotterFact = db.define("potterfact", {
-  fact: { type: Sequelize.STRING }
+  fact: { type: Sequelize.TEXT }
 });
 
 module.exports = PotterFact;
