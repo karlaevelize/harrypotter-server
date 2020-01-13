6904e34858eb53fdd76d989e1f7fcfe628b8e152
karaevelize <k.evelize@gmail.com>
2020-01-13 12:00:16 +0100

    Added the router on the index page, it works

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const bodyParser = require("body-parser");
 const Sse = require("json-sse");
 const stream = new Sse();
 
+const PotterMessage = require("./pottermessages/model");
+
 const potterFactRouter = require("./potterfacts/router");
 const potterQuoteRouter = require("./potterquotes/router");
 const potterHeadRouter = require("./potterheads/router");
 const authRouter = require("./auth/router");
+const potterMessageRouterFactory = require("./pottermessages/router");
 
 const port = process.env.PORT || 4002;
 const app = express();
 
 const corsMiddleware = cors();
 const parserMiddleware = bodyParser.json();
+const potterMessageRouter = potterMessageRouterFactory(stream);
 
 app.use(corsMiddleware);
 app.use(parserMiddleware);
@@ -22,5 +26,25 @@ app.use(potterFactRouter);
 app.use(potterQuoteRouter);
 app.use(potterHeadRouter);
 app.use(authRouter);
+app.use(potterMessageRouter);
+
+app.get("/", (request, response) => {
+  response.send("hello");
+});
+
+app.get("/stream", async (request, response, next) => {
+  try {
+    const messages = await PotterMessage.findAll();
+    const action = {
+      type: "ALL_MESSAGES",
+      payload: messages
+    };
+    const string = JSON.stringify(action);
+    stream.updateInit(string);
+    stream.init(request, response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(port, () => console.log(`Hey, I'm on port ${port}!`));
